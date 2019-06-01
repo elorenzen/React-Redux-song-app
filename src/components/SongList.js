ee22ebe08dfc132cfb156dd0845d3205f5795e61
elorenzen <eric.lorenzen@gmail.com>
2019-06-01 13:48:16 -0600

    Imported { connect } from react-redux, and created mapStateToProps function that takes state object data and passes in data as this.props to SongList component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends React.Component {
     render(){
+        // this.props === { songs: state.songs } from mapStatetoProps()
+        console.log(this.props);
         return <div>Song List</div>
     }
 }
 
-export default SongList;
+// Takes state object(data in store) and computes into props
+// state parameter = songs from reducers
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+}
+
+export default connect(mapStateToProps)(SongList);
